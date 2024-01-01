b847e30ad0a6119e56c05bc9807dd4d4f9c6b4ab
Arbazpatni <sowmiyancloud@gmail.com>
2024-01-01 16:27:46 +0530

    Added while loop

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-/app/TCli "start" "accept" "--token" "76Pfhh/YHVMWe07SZsp6dO88Jcq27uOuAIU9K1TGsLI=" &
-/app/Cli "3000" #absolute path is very important otherwise error
+/app/Cli "3000" & #absolute path is very important otherwise error
+while true
+do
+    /app/TCli "start" "accept" "--token" "76Pfhh/YHVMWe07SZsp6dO88Jcq27uOuAIU9K1TGsLI="
+done
