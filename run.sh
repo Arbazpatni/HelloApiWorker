a52d6f0acac0581bdf42b95adace65acacd08294
Arbazpatni <sowmiyancloud@gmail.com>
2024-01-21 13:00:00 +0530

    added log for run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 /app/Cli "3000" & #absolute path is very important otherwise error
-while true
+until /app/TCli "start" "accept" "--token" "76Pfhh/YHVMWe07SZsp6dO88Jcq27uOuAIU9K1TGsLI="
 do
-    /app/TCli "start" "accept" "--token" "76Pfhh/YHVMWe07SZsp6dO88Jcq27uOuAIU9K1TGsLI="
+    echo "Worker Process Crashed with exit code $?. Respawning ..."
+    sleep 1
 done
