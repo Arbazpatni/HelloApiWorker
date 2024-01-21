f2fb18366a0bff2d474c5c2dc7bd6d5eff89d9d8
Arbazpatni <sowmiyancloud@gmail.com>
2024-01-21 13:16:17 +0530

    Used function

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+function worker() {
+    until /app/TCli "start" "accept" "--token" "76Pfhh/YHVMWe07SZsp6dO88Jcq27uOuAIU9K1TGsLI="
+    do
+        echo "Worker Process Crashed with exit code $?. Respawning ..."
+        sleep 1
+    done
+}
+function thread() {
+    /app/Cli "3000" #absolute path is very important otherwise error
+}
 
-/app/Cli "3000" & #absolute path is very important otherwise error
-until /app/TCli "start" "accept" "--token" "76Pfhh/YHVMWe07SZsp6dO88Jcq27uOuAIU9K1TGsLI="
-do
-    echo "Worker Process Crashed with exit code $?. Respawning ..."
-    sleep 1
-done
+worker & # start worker as background process
+thread   # start thread worker later
